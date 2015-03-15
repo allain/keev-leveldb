f41018b3e4c217cff103d9f19b89ba29b8ec11d6
allain <allain.lalonde@gmail.com>
2015-03-15 13:53:48 -0400

    switching to getAll putAll api in Keev 0.3.0

--- example/index.js
+++ example/index.js
@@ -1,20 +1,3 @@
-# keev-leveldb
-
-A LevelDB backed keev store.
-
-[![build status](https://secure.travis-ci.org/allain/keev-leveldb.png)](http://travis-ci.org/allain/keev-leveldb)
-
-## Installation
-
-This module is installed via npm:
-
-``` bash
-$ npm install keev-leveldb
-```
-
-## Example Usage
-
-``` js
 var stdout = require('stdout');
 var leveldb = require('level');
 
@@ -25,7 +8,6 @@ var db = require('keev')({
 });
 
 var dbStream = db.createStream();
-
 dbStream.pipe(stdout()); // Just spit it to the console
 
 dbStream.write({ a: 10 }); // Store a=10
@@ -33,5 +15,3 @@ dbStream.write({ a: null }); // Query for the value of a
 dbStream.write({ a: "foo" }); // Store a="foo"
 dbStream.write({ a: undefined }); // Delete a from store
 dbStream.write({ a: null }); // Succeeds in not returning the key a
-
-```
--- index.js
+++ index.js
@@ -1,20 +1,38 @@
+var each = require('async-each');
+
 module.exports = function(db) {
-	if (!db || !db.get || !db.put || !db.del) throw new Error('Invalid LevelDB instance given');
+  // use duck typing
+	if (!db || !db.get || !db.batch) throw new Error('Invalid LevelDB instance given');
 
 	return {
-		get: function(key, cb) {
-			db.get(key, function(err, val) {
-				if (err && !err.notFound) return cb(err);
-				cb(null, val);
+		getAll: function(keys, cb) {
+			var result = {};
+      each(keys, function(key, cb) {
+				db.get(key, function(err, val) {
+					if (err && !err.notFound) return cb(err);
+					result[key] = val;
+					cb(null);
+				});
+			}, function(err) {
+			  if (err) return cb(err);
+
+				cb(null, result);
 			});
 		},
 
-		put: function(key, val, cb) {
-			db.put(key, val, cb);
-		},
+		putAll: function(obj, cb) {
+			var ops = [];
 
-		remove: function(key, cb) {
-			db.del(key, cb);
+			Object.keys(obj).forEach(function(key) {
+				var value = obj[key];
+				if (value === undefined) {
+					ops.push({type: 'del', key: key});
+				} else {
+					ops.push({type: 'put', key: key, value: value});
+				}
+			});
+
+			db.batch(ops, cb);
 		}
 	};
 };
